e82700deb0870a05d922c69ca7be466ccda6128e
Enmanuel Carrasquel <enmanuelcarrasquel@ballastlane.com>
2020-04-21 21:52:31 -0400

    Changed port

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const helmet = require('helmet')
 const port = 3000
 
+app.use(helmet())
 app.use(express.static('public'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(process.env.PORT || 3000, () => console.log(`Example app listening at http://localhost:${port}`))
